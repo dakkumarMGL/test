da0b77c63bf4d535402bd715a82c88a5e04b51fe
dakkumarMGL <arun@microgridlabs.com>
2021-06-10 10:56:40 -0400

    Create dictionaryKeys.py
    This code checks to see if a dictionary is empty.

---
+++ dictionaryKeys.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  9 20:11:14 2021
+
+@author: arunkumar
+"""
+
+
+mydict = {1:['a', 'b', 'c'], 2:['d', 'e', 'f']}
+#mydict = {}
+
+mykeys = []
+for key in mydict.keys():
+    mykeys.append(key)
+    
+print(bool(mydict))
