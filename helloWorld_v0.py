dac78bdb7d8fda62a6c237c48c92c0bbac0470ac
dakkumarMGL <arun@microgridlabs.com>
2021-08-02 13:09:19 -0400

    Create helloWorld_v0.py

---
+++ helloWorld_v0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  2 13:03:21 2021
+
+@author: arunkumar
+"""
+
+def main():
+    
+    print('Hello world!')
+    
+    
+    
+if __name__ == '__main__': main()
+
